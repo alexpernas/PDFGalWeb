6b6dd8693c377ca123f1c377ddd862c2dafa8cbe
apernas <alexpernas@gmail.com>
2014-06-18 10:46:21 +0200

    Modification of split method and FileUtils class. Modificación do método split e da clase FileUtils.

--- PDFGalWeb/src/main/java/org/pdfgal/pdfgalweb/utils/FileUtils.java
+++ PDFGalWeb/src/main/java/org/pdfgal/pdfgalweb/utils/FileUtils.java
@@ -19,6 +19,13 @@ public interface FileUtils {
 	boolean delete(String uri);
 
 	/**
+	 * Deletes the files existing in the paths included in the parameter's list.
+	 * @param urisList
+	 * @return
+	 */
+	boolean delete(List<String> urisList);
+
+	/**
 	 * Returns the original name received as parameter with 20 auto generated
 	 * characters before the original name.
 	 * 
@@ -31,35 +38,27 @@ public interface FileUtils {
 	 * Prepares the {@link HttpServletResponse} for downloading the file whose
 	 * URI is the received parameter.
 	 * 
-	 * @param response
-	 *            The HttpServletResponse.
-	 * @param uri
-	 *            The URI of the file to download.
-	 * @param fileName
-	 *            The file name of the file to download.
+	 * @param response The HttpServletResponse.
+	 * @param uri The URI of the file to download.
+	 * @param fileName The file name of the file to download.
 	 * @throws IOException
 	 */
-	void prepareFileDownload(HttpServletResponse response, String uri,
-			String fileName) throws IOException;
+	void prepareFileDownload(HttpServletResponse response, String uri, String fileName)
+			throws IOException;
 
 	/**
 	 * 
-	 * @param response
-	 *            The HttpServletResponse.
-	 * @param urisList
-	 *            The URIs of the files to download.
-	 * @param fileName
-	 *            The file name of the files to download.
+	 * @param response The HttpServletResponse.
+	 * @param urisList The URIs of the files to download.
+	 * @param fileName The file name of the files to download.
 	 */
-	void prepareFileDownload(HttpServletResponse response,
-			List<String> urisList, String fileName);
+	void prepareFileDownload(HttpServletResponse response, List<String> urisList, String fileName);
 
 	/**
 	 * Stores the {@link MultipartFile} to the system. Returns the path where
 	 * the file was stored, or null in case it could not be stored.
 	 * 
-	 * @param file
-	 *            File to save.
+	 * @param file File to save.
 	 * @return Path where file was stored, or null in case file could not be
 	 *         stored.
 	 */
