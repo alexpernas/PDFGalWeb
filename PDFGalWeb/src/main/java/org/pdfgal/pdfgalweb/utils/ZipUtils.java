c9aa5ba6a48ca0b159c73e0391fc0db51faf9597
Alex <Alex@AlexPCPort>
2014-06-20 00:47:49 +0200

    Split page finished (without error control and styles). Rematada a páxina de división (sen control de erros nin estilos).

--- PDFGalWeb/src/main/java/org/pdfgal/pdfgalweb/utils/ZipUtils.java
+++ PDFGalWeb/src/main/java/org/pdfgal/pdfgalweb/utils/ZipUtils.java
@@ -9,11 +9,13 @@ public interface ZipUtils {
 	/**
 	 * Saves the files located at URIs set on the urisList into a new ZIP file,
 	 * named fileName.
+	 * 
 	 * @param urisList
 	 * @param fileName
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-	void zipFiles(List<String> urisList, String fileName) throws FileNotFoundException, IOException;
+	String zipFiles(List<String> urisList, String fileName)
+			throws FileNotFoundException, IOException;
 
 }
